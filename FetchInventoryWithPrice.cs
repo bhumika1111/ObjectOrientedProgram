35c70083824d361a8d2c27c492a011cbc780acb8
bhumika1111 <bhoyarpatil@434>
2022-08-10 23:50:06 +0530

    *Added Fetch Inventry With Price*

---
+++ FetchInventoryWithPrice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace InventoryManagement
+{
+   public class FetchInventoryWithPrice
+    {
+        public Rice Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Rice>(json);
+                }
+                catch (Exception)
+                {
+                    return null;
+
+                }
+            }
+
+        }
+    }
+
+}

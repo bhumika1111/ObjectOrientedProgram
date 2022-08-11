fc09eca9ab11d8125f774f9773566bfb1748857f
bhumika1111 <bhoyarpatil@434>
2022-08-11 16:39:47 +0530

    *Added Stock Fetch Program File*

---
+++ FetchStock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace StockAccountManagement
+{
+    public class FetchStock
+    {
+        public StockDetails Read(string path)
+        {
+           using  (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<StockDetails>(json);
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
+
+

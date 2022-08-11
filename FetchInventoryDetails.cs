f3356976a17c9dfc438121e608184533570e70c9
bhumika1111 <bhoyarpatil@434>
2022-08-11 16:51:10 +0530

    *Added FetchInventory Details Program File*

---
+++ FetchInventoryDetails.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace InventoryManagement
+{
+    public class FetchInventoryDetails
+    {
+
+        public Rice Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Rice>(json);
+                }
+                catch (Exception)
+                {
+                    return null;
+
+                }
+            }
+        }
+
+
+
+    }
+}

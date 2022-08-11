945437813a9e8df290401d4d2dc87f27cb917b5d
bhumika1111 <bhoyarpatil@434>
2022-08-11 16:41:13 +0530

    *Added Stock Details Program File*

---
+++ StockDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockAccountManagement
+{
+    public class StockDetails
+    {
+
+        public List<TypesOfCompaniesForShares> typesofStocks;
+
+    }
+
+    public class TypesOfCompaniesForShares
+    {
+        public string ShareName;
+        public int NumberofShare;
+        public int SharePrice;
+    }
+
+
+
+    
+}

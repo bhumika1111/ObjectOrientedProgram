29056ba235ba090a8a12c73563c079c698831b9f
bhumika1111 <bhoyarpatil@434>
2022-08-11 16:48:36 +0530

    *Added Inventory Data Management Program Main File*

--- Program.cs
+++ Program.cs
@@ -8,49 +8,42 @@ namespace InventoryManagement
 
         public static void Main(string[] args)
         {
-            FetchInventoryWithPrice fetchInventoryDetails = new FetchInventoryWithPrice();
+            FetchInventoryDetails fetchInventoryDetails = new FetchInventoryDetails();
             string filepath = "C:\\Users\\User\\source\\repos\\InventoryManagement\\InventoryManagement\\Data.json";
 
-            Rice data = fetchInventoryDetails.Read(filepath);
+           Rice data = fetchInventoryDetails.Read(filepath);
+
+            Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("*****Types of Rice********");
+            Console.WriteLine("-----------------------------");
             for (int  i = 0; i < data.typesofRice.Count; i++)
             {
                 Console.WriteLine(data.typesofRice[i].name);
                 Console.WriteLine(data.typesofRice[i].weight);
                 Console.WriteLine(data.typesofRice[i].price);
-
-                
-
-
-                int total = data.typesofRice[i].price * data.typesofRice[i].weight;
-                Console.WriteLine("The price of " + data.typesofRice[i].name + "for " + data.typesofRice[i].weight + "=" + total);
-                Console.WriteLine("The price of " + data.typesofRice[i].name + "for " + "kg = " + total);
-
-
-                Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+                Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::");
             }
+
+            Console.WriteLine("*****Types of Pulses********");
+            Console.WriteLine("-----------------------------");
             for (int  i = 0; i < data.typesofPulses.Count; i++)
             {
                 Console.WriteLine(data.typesofPulses[i].name);
                 Console.WriteLine(data.typesofPulses[i].weight);
                 Console.WriteLine(data.typesofPulses[i].price);
-
-                int total = data.typesofPulses[i].price * data.typesofPulses[i].weight;
-                Console.WriteLine("The price of " + data.typesofPulses[i].name + "for " + data.typesofPulses[i].weight + "=" + total);
-                Console.WriteLine("The price of " + data.typesofWheat[i].name + "for " + "kg = " + total);
-                Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+                Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::");
 
 
             }
+
+            Console.WriteLine("*******Types of Wheat*********");
+            Console.WriteLine("------------------------------");
             for (int i = 0; i < data.typesofWheat.Count; i++)
             {
                 Console.WriteLine(data.typesofWheat[i].name);
                 Console.WriteLine(data.typesofWheat[i].weight);
                 Console.WriteLine(data.typesofWheat[i].price);
-
-                int total = data.typesofWheat[i].price * data.typesofWheat[i].weight;
-                Console.WriteLine("The price of " + data.typesofWheat[i].name + "for " + data.typesofWheat[i].weight + "=" + total);
-                Console.WriteLine("The price of " + data.typesofWheat[i].name + "for " + "kg = " + total);
-                Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+                Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::::");
 
 
             }
